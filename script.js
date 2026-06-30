364732d44f67198ca4b6340bac9c4404eff6a597
christyshiran10-spec <christyshiran10@gmail.com>
2026-06-30 20:08:19 +0530

    feat: add localstorage-backed dark and light mode toggle switch

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ document.getElementById('contactForm')?.addEventListener('submit', function(e) {
     alert('Thank you! Message captured successfully.');
     this.reset();
 });
+const themeToggleBtn = document.getElementById('themeToggle');
+const themeIcon = document.getElementById('themeIcon');
+
+// කලින් යූසර් සිලෙක්ට් කරපු තීම් එකක් තියෙනවද බලන්න
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-mode');
+    themeIcon.classList.replace('fa-moon', 'fa-sun');
+}
+
+
+themeToggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    
+    if (document.body.classList.contains('dark-mode')) {
+        themeIcon.classList.replace('fa-moon', 'fa-sun');
+        localStorage.setItem('theme', 'dark'); 
+    } else {
+        themeIcon.classList.replace('fa-sun', 'fa-moon');
+        localStorage.setItem('theme', 'light'); 
+    }
+});
